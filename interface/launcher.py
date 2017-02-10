bb5991908c7de62f6efcb47f738142a65f0a93bb
Cosimo89 <cosimo.livi@gmail.com>
2017-02-10 18:27:20 +0100

    all aftenoon in trying to understand why c func calls have no effects on Structures

--- interface/launcher.py
+++ interface/launcher.py
@@ -1,30 +1,22 @@
 from ctypes import *
-from numpy import *
+#from numpy import *
 from sys import *
 import os
 
 cwd=os.getcwd()
 lib_d=cwd+'/lib'
-print lib_d
-
-clibs=CDLL(''+lib_d+'/all_in.so')
-
-"""
-data = CDLL(''+lib_d+'/datafile.so')
-helpers = CDLL(''+lib_d+'/helpers.so')
-force = CDLL(''+lib_d+'/force.so')
-velverlet = CDLL(''+lib_d+'/velverlet.so')
-
-output = CDLL(''+lib_d+'/output.so')
-input_ = CDLL(''+lib_d+'/input.so')
-"""
 
 
-class _mdsys(Structure):
-    [("natoms",c_int),("mass",c_double),("epsilon", c_double),\
-    ("sigma",c_double),("rcut",c_double),("box_lenght",c_double),\
-    ("restart",c_char_p),("trajectory",c_char_p),("energies",c_char_p),\
-    ("MD_steps", c_double),("MD_time_steps",c_int),("output_print_freq",c_int),\
+clibs=CDLL(''+lib_d+'/libljmd.so')
+
+#("restart",c_char_p),("trajectory",c_char_p),("energies",c_char_p),\
+
+class mdsys_t (Structure):
+    pass
+
+mdsys_t .__fields__ =[("natoms",c_int),("nfi",c_int),("nsteps",c_int),\
+    ("dt", c_double),("mass",c_double),("epsilon", c_double),\
+    ("sigma",c_double),("box",c_double),("rcut",c_double),\
     ("ekin",c_double),("epot",c_double),("temp",c_double),\
     ("rx", c_double),("ry",c_double),("rz",c_double),\
     ("vx", c_double),("vy",c_double),("vz",c_double),\
@@ -38,29 +30,71 @@ with open(inFile,'r') as inp:
     for el in inp:
         line=el.partition('#')[0]
         lines.append(line)
-
-sys=_mdsys(\
+sys=mdsys_t (\
 natoms=(int)(lines[0]),\
+nfi=(int)(0),\
+nsteps=(int)(lines[9]),\
+dt=(float)(lines[10]),\
 mass=(float)(lines[1]),\
 epsilon=(float)(lines[2]),\
 sigma=(float)(lines[3]),\
 rcut=(float)(lines[4]),\
-box_lenght=(float)(lines[5]),\
-restart=lines[6],\
+box=(float)(lines[5]),\
+ekin=(float)(0),\
+epot=(float)(0),\
+temp=(float)(0)
+)
+nprints=(int)(lines[11])
+"""
+restart=(str)(lines[6].partition(' ')[0]),\
 trajectory=lines[7],\
 energies=lines[8],\
-MD_steps=(int)(lines[9]),\
-MD_time_steps=(float)(lines[10]),\
-output_print_freq=(int)(lines[11])\
-)
-
-with open(inFile,'r') as inp:
-    for el in inp:
-        line=el.partition('#')[0]
-        lines.append(line)
-
-
-print type(sys.natoms)
+"""
+sys.rx=(c_double * sys.natoms)()
+sys.ry=(c_double * sys.natoms)()
+sys.rz=(c_double * sys.natoms)()
+sys.vx=(c_double * sys.natoms)()
+sys.vy=(c_double * sys.natoms)()
+sys.vz=(c_double * sys.natoms)()
+sys.fx=(c_double * sys.natoms)()
+sys.fy=(c_double * sys.natoms)()
+sys.fz=(c_double * sys.natoms)()
 
 
-#sys=_mdsys(inp[0,0])
+# Read restart
+rest=open(cwd+'/'+(lines[6].partition(' ')[0]),"r")
+if (rest):
+    i=0
+    j=0
+    for line in rest:
+        if (i<sys.natoms):
+
+            data=line.split()
+            sys.rx[i]=(float)(data[0])
+            sys.ry[i]=(float)(data[1])
+            sys.rz[i]=(float)(data[2])
+            i+=1
+        else:
+            data=line.split()
+            sys.vx[j]=(float)(data[0])
+            sys.vy[j]=(float)(data[1])
+            sys.vz[j]=(float)(data[2])
+            j+=1
+    rest.close()
+    clibs.azzero(sys.fx, sys.natoms)
+    clibs.azzero(sys.fy, sys.natoms)
+    clibs.azzero(sys.fz, sys.natoms)
+
+
+else:
+    print "cannot read restart file"
+
+
+# initialize forces and energies
+sys.nfi=0
+print "before force call: sys.rx[0]=  %f" % sys.rx[0]
+
+clibs.force.argtypes=[POINTER(mdsys_t)]
+clibs.force(byref(sys))
+print "after force call: sys.rx[0]=  %f" % sys.rx[0]
+print "C force call does not affects sys..."
