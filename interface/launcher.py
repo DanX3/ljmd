6f2650667a7329b0576611a911fe6eb629009730
Cosimo89 <cosimo.livi@gmail.com>
2017-02-10 11:41:33 +0100

    old Makefile, must update from repo

--- interface/launcher.py
+++ interface/launcher.py
@@ -1,26 +1,33 @@
 from ctypes import *
 from numpy import *
 from sys import *
-"""
-data = CDLL('./lib/data.so')
-force = CDLL('./lib/force.so')
-vel = CDLL('./lib/ververlet.so')
-helpers = CDLL('./lib/helpers.so')
-output = CDLL('./lib/output.so')
-input_ = CDLL('./lib/input_.so')
+import os
+
+cwd=os.getcwd()
+lib_d=cwd+'/lib'
+print lib_d
+
+data = CDLL(''+lib_d+'/datafile.so')
+helpers = CDLL(''+lib_d+'/helpers.so')
+force = CDLL(''+lib_d+'/force.so')
+velverlet = CDLL(''+lib_d+'/velverlet.so')
+
+output = CDLL(''+lib_d+'/output.so')
+input_ = CDLL(''+lib_d+'/input.so')
 
 
-"""
+
 class _mdsys(Structure):
     [("natoms",c_int),("mass",c_double),("epsilon", c_double),\
     ("sigma",c_double),("rcut",c_double),("box_lenght",c_double),\
     ("restart",c_char_p),("trajectory",c_char_p),("energies",c_char_p),\
-    ("MD_steps", c_double),("MD_time_steps",c_int),("output_print_freq",c_int),\
+    ("MD_steps", c_double),("MD_time_steps",c_int),("output_print_freq",c_int)]
+"""
     ("ekin",c_double),("epot",c_double),("temp",c_double),\
     ("rx", c_double),("ry",c_double),("rz",c_double),\
     ("vx", c_double),("vy",c_double),("vz",c_double),\
     ("fx", c_double),("fy",c_double),("fz",c_double)]
-
+"""
 
 
 inFile=argv[1]
