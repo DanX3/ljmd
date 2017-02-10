325a50a4c676092864a1d98e58c13e6f22c02d3c
Cosimo89 <cosimo.livi@gmail.com>
2017-02-10 10:39:45 +0100

    implemented input

--- interface/launcher.py
+++ interface/launcher.py
@@ -15,7 +15,7 @@ class _mdsys(Structure):
     [("natoms",c_int),("mass",c_double),("epsilon", c_double),\
     ("sigma",c_double),("rcut",c_double),("box_lenght",c_double),\
     ("restart",c_char_p),("trajectory",c_char_p),("energies",c_char_p),\
-    ("MD steps", c_double),("MD time steps",c_int),("output print freq",c_int)]
+    ("MD_steps", c_double),("MD_time_steps",c_int),("output_print_freq",c_int)]
 
 
 inFile=argv[1]
@@ -25,9 +25,24 @@ with open(inFile,'r') as inp:
         line=el.partition('#')[0]
         lines.append(line)
 
-sys=_mdsys(natoms=
-
-
+sys=_mdsys(\
+natoms=lines[0],\
+mass=lines[1],\
+epsilon=lines[2],\
+sigma=lines[3],\
+rcut=lines[4],\
+box_lenght=lines[5],\
+restart=lines[6],\
+trajectory=lines[7],\
+energies=lines[8],\
+MD_steps=lines[9],\
+MD_time_steps=lines[10],\
+output_print_freq=lines[11]\
 )
+
+
+
+print sys.natoms
+
 
 #sys=_mdsys(inp[0,0])
